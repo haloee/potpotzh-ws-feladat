21544cc7ea03f9d8cb2304ac018019a5ded47f04
Sztaracsek Matyas <sztaracsekmatyas@gmail.com>
2025-05-20 14:11:17 +0200

    service es soap modositas

--- src/main/java/hu/pte/mik/prog4/ws/CompanyDataService.java
+++ src/main/java/hu/pte/mik/prog4/ws/CompanyDataService.java
@@ -2,9 +2,12 @@ package hu.pte.mik.prog4.ws;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
 import jakarta.jws.soap.SOAPBinding;
+import jakarta.jws.soap.SOAPBinding.Style;
 
 @WebService
-@SOAPBinding(style = SOAPBinding.Style.DOCUMENT)
+@SOAPBinding(
+        style = Style.DOCUMENT
+)
 public interface CompanyDataService {
     @WebMethod
     CompanyDataResponse getCompanyData(CompanyDataRequest request);
--- src/main/java/hu/pte/mik/prog4/ws/SoapCompanyDataService.java
+++ src/main/java/hu/pte/mik/prog4/ws/SoapCompanyDataService.java
@@ -1,17 +1,16 @@
 package hu.pte.mik.prog4.ws;
 import jakarta.jws.WebService;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 @WebService(endpointInterface = "hu.pte.mik.prog4.CompanyDataService",
         serviceName = "CompanyDataService",
-        targetNamespace = "hu.pte.mik.prog4.ws.soap")
+        targetNamespace = "http://ws.hu.pte.mik.prog4")
 public class SoapCompanyDataService implements CompanyDataService {
     private final Map<String, Double> map = new HashMap();
     public SoapCompanyDataService() {
     }
-    @Override
+
     public CompanyDataResponse getCompanyData(CompanyDataRequest request) {
         return new CompanyDataResponse(request.getCompanyId(), this.map.computeIfAbsent(request.getCompanyId(), (companyId) -> ThreadLocalRandom.current().nextDouble(0.0, 100.0)));
     }
